35f2204424463672ad5b55fafe85e824ef70fb6f
Maurício Dévé <134610994+devemauricio@users.noreply.github.com>
2023-06-15 08:15:23 -0300

    Update cadastro_alunos_linkedlist.py

--- cadastro_alunos_linkedlist.py
+++ cadastro_alunos_linkedlist.py
@@ -34,16 +34,32 @@ class LinkedList():
     else:
       current = self.head
       for _ in range(index-1):
-        if current.noext is not None:
+        if current.next is not None:
           current = current.next
         else:
           break
       new_node.next = current.next
       current.next = new_node
+  def remove_index(self, index):
+    if index == 0:
+      if self.head is not None:
+        self.head = self.head.next
+    else:
+      current = self.head
+      for _ in range(index - 1):
+        if current is not None:
+          current = current.next
+        else:
+          break
+      if current is not None and current.next is not None:
+        current.next = current.next.next
+
 
 lista = LinkedList()
 lista.append("12345678900", "Maurício Dévé", [10, 2.5, 3.7, 4.5, 10])
 lista.append("32260109123", "John Lennon", [7.8, 6.4, 8.8, 7.0, 8.0])
 lista.append("19621963196", "Paul McCartney", [0.5, 0.8, 10.0, 5.0, 6.5])
 lista.append_index("40028922", "User123", [1, 2, 3, 4, 5], 1)
+lista.printList()
+lista.remove_index(1)
 lista.printList()
