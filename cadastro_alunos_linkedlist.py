b7bbe530cd978a174c29866884cab75719adde5d
Maurício Dévé <134610994+devemauricio@users.noreply.github.com>
2023-06-15 08:10:36 -0300

    Update cadastro_alunos_linkedlist.py

--- cadastro_alunos_linkedlist.py
+++ cadastro_alunos_linkedlist.py
@@ -3,7 +3,7 @@ class Node():
     self.matricula = matricula
     self.nome = nome
     self.list_notas = ["Português", "Matemática", "Física", "Biologia", "Gramática"]
-    self.notas = {materia: float(nota) for materia in self.list_notas for nota in notas}
+    self.notas = {materia: float(nota) for materia, nota in zip(self.list_notas, notas)}
     self.next = None
 
 class LinkedList():
@@ -26,8 +26,24 @@ class LinkedList():
         print(f"Matrícula: {current.matricula} - Nome: {current.nome} - Notas: {current.notas}")
         current = current.next
 
+  def append_index(self, matricula, nome, notas, index):
+    new_node = Node(matricula, nome, notas)
+    if index == 0:
+      new_node.next = self.head
+      self.heead = new_node
+    else:
+      current = self.head
+      for _ in range(index-1):
+        if current.noext is not None:
+          current = current.next
+        else:
+          break
+      new_node.next = current.next
+      current.next = new_node
+
 lista = LinkedList()
 lista.append("12345678900", "Maurício Dévé", [10, 2.5, 3.7, 4.5, 10])
 lista.append("32260109123", "John Lennon", [7.8, 6.4, 8.8, 7.0, 8.0])
 lista.append("19621963196", "Paul McCartney", [0.5, 0.8, 10.0, 5.0, 6.5])
+lista.append_index("40028922", "User123", [1, 2, 3, 4, 5], 1)
 lista.printList()
