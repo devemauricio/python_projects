67a2b7a908021e365256ea95ccce56653fcb2473
Maurício Dévé <134610994+devemauricio@users.noreply.github.com>
2024-01-22 12:35:37 -0300

    Update jogo-das-3-portas.py

--- jogo-das-3-portas.py
+++ jogo-das-3-portas.py
@@ -1,6 +1,6 @@
 #jogo das 3 portas
 #há três portas
-#atrás de duas delas há um carro, e nas outras duas uma cabra
+#atrás de uma delas há um carro, e nas outras duas uma cabra
 #o usuário escolhe uma porta
 #o apresentador, que sabe onde está o carro, abre uma outra porta onde está uma cabra
 #o usuário tem a opção de manter a escolha ou mudar de porta
